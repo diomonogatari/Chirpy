a7f43e09d30346c828597f499408d4d8cd6c8d73
diomonogatari <dcarvalho.professional@hotmail.com>
2024-09-21 15:19:42 +0100

    add metrics and stateless middleware with hit count

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func main() {
+	var apiConfig ApiConfig
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", myHandler)
+
+	wrappedHandler := apiConfig.incrementHits(http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", wrappedHandler)
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiConfig.getHits)
+	mux.HandleFunc("/reset", apiConfig.resetHits)
 
 	server := &http.Server{
 		Handler: mux,
@@ -17,8 +23,34 @@ func main() {
 	server.ListenAndServe()
 }
 
-func myHandler(response http.ResponseWriter, request *http.Request) {
+func healthzHandler(response http.ResponseWriter, request *http.Request) {
 	response.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	response.WriteHeader(200)
 	response.Write([]byte("OK"))
+}
+
+type ApiConfig struct {
+	fileserverHits int
+}
+
+func (cfg *ApiConfig) incrementHits(next http.Handler) http.Handler {
+	incr := func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(incr)
+}
+
+func (cfg *ApiConfig) getHits(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("Hits: " + fmt.Sprint(cfg.fileserverHits)))
+}
+
+func (cfg *ApiConfig) resetHits(w http.ResponseWriter, _ *http.Request) {
+	cfg.fileserverHits = 0
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(200)
 }
